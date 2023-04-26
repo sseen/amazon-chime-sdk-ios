957a1a94832c489f959f4d9bf6e137a5037b3524
Dingyi Shen <dingyshe@amazon.com>
2023-04-26 14:12:27 -0700

    set remote url

--- Package.swift
+++ Package.swift
@@ -20,15 +20,18 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "AmazonChimeSDK",
-            path: "Sources/AmazonChimeSDK.xcframework"
+            url: "https://spm-dingyshe.s3.us-west-2.amazonaws.com/AmazonChimeSDK-0.23.0.zip",
+            checksum: "ac54d52fd33082c2436bdfdf090be3c1414c1bc585190c2e4bad01e08464a0ce"
         ),
         .binaryTarget(
             name: "AmazonChimeSDKMedia",
-            path: "Sources/AmazonChimeSDKMedia.xcframework"
+            url: "https://spm-dingyshe.s3.us-west-2.amazonaws.com/AmazonChimeSDKMedia-0.18.0.zip",
+            checksum: "4c013ae42339ce6bf9824a79ecf6ac8ce637584419bc4150e3439f214beb1979"
         ),
         .binaryTarget(
             name: "AmazonChimeSDKMachineLearning",
-            path: "Sources/AmazonChimeSDKMachineLearning.xcframework"
+            url: "https://spm-dingyshe.s3.us-west-2.amazonaws.com/AmazonChimeSDKMachineLearning-0.2.0.zip",
+            checksum: "4a542758b6c7f606d9a934ccd87c752e90c74c235f4a978447cce1ff677ec657"
         )
     ]
 )
